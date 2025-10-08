8d7b3a0e9509e63100c0d2df645c7dde0c7c4f5a
Jesus David Mejia Vergara <davidmejia-2000@outlook.com>
2025-10-07 20:22:45 -0500

    feat: Seccion de relacion entre colecciones y autores

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection, reference } from "astro:content";
 
 const blogCollection = defineCollection({
     type: 'content',
@@ -6,16 +6,34 @@ const blogCollection = defineCollection({
         title: z.string(),
         date: z.date(),
         description: z.string(),
-        image: image().refine((img) => img),
+        image: image(),
         
         //Relación
-        author: z.string(),
+        //author: z.string(),
+        author: reference('author'),
         
         //Relación
         tags: z.array(z.string()),
+
+        //Boolean
+        isDraft: z.boolean().default(false),
     })
 });
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+        x: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+    })
+})
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 }
